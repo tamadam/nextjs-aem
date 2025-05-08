83c88b97be2542aed584cb61747d1979f71daa21
Adam <adam.tamas@ni.com>
2025-05-08 15:33:20 +0200

    add tmp components

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,26 @@ export default async function Home() {
       Next.js Static Content
       {data ? "There is data" : "No data"}
       {/*  <Container data={data.container} /> */}
+      <div
+        data-aue-type="container"
+        data-aue-label="Container"
+        data-aue-resource={`urn:aemconnection:/content/test-ue-page/jcr:content/container`}
+        style={{
+          padding: "1rem",
+          border: "1px solid red",
+          marginBlock: "1rem",
+        }}
+      >
+        <div
+          data-aue-resource="urn:aemconnection:/content/test-ue-page/jcr:content/container/text_comp"
+          data-aue-prop="text"
+          data-aue-type="richtext"
+          data-aue-label="Text component"
+          data-aue-behavior="component"
+        >
+          Editable box
+        </div>
+      </div>
     </div>
   );
 }
