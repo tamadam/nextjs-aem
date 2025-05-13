59d07bc2eb93d157defb13503ae69cfeed3f7667
Adam <adam.tamas@ni.com>
2025-05-13 10:30:56 +0200

    remove old getaemdata

--- lib/aem-service.ts
+++ lib/aem-service.ts
@@ -20,41 +20,6 @@ export async function getAemData(slugPath: string) {
       cache: "no-store",
     });
 
-    if (!res.ok) {
-      throw new Error(
-        `Failed to fetch AEM data ${res.status} ${res.statusText}`
-      );
-    }
-
-    return res.json();
-  } catch (error) {
-    console.error("Error fetching AEM data:", error);
-    return null;
-  }
-}
-
-export function getContainerPath(slugPath: string) {
-  return `/content/${slugPath}/jcr:content/container`;
-}
-
-export async function getAemDataOld() {
-  const token = process.env.TOKEN;
-  try {
-    const res = await fetch(
-      process.env.LOCAL === "1"
-        ? "https://localhost:8443/content/test-ue-page.model.json"
-        : "https://author-p12074-e30717.adobeaemcloud.com/content/test-ue-page.model.json",
-      {
-        headers: {
-          Authorization:
-            process.env.LOCAL === "1"
-              ? "Basic " + Buffer.from("admin:admin").toString("base64")
-              : `Bearer ${token}`,
-        },
-        cache: "no-store",
-      }
-    );
-
     if (res.status === 404) {
       return { notFound: true };
     }
@@ -71,4 +36,8 @@ export async function getAemDataOld() {
     console.error("Error fetching AEM data:", error);
     return { error: true };
   }
+}
+
+export function getContainerPath(slugPath: string) {
+  return `/content/${slugPath}/jcr:content/container`;
 }
