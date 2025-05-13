c7bf581b306d614478c2cb72c6a8054f6186e358
Adam <adam.tamas@ni.com>
2025-05-13 10:32:15 +0200

    add middleware to redirect from / to /nexthome

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/nexthome";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
